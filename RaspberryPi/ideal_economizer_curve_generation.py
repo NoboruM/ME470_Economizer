e8a210d57a88b8ec02746092b572757746cf1b6d
NoboruM <99701230+NoboruM@users.noreply.github.com>
2024-03-25 00:38:00 -0500

    Update ideal_economizer_curve_generation.py
    added random data to plot

--- RaspberryPi/ideal_economizer_curve_generation.py
+++ RaspberryPi/ideal_economizer_curve_generation.py
@@ -50,3 +50,45 @@ def plot_curve_w_user_inputs_and_raw_data(graphed_raw_data, minimum_percent_outs
     plot = plot_curve_on_exisiting_plot(graphed_raw_data, oat, actual_mat)
     plot.show()
     # return plot
+
+def PlotIdealCurve(min_outside_air, rat, low_lockout_temp, high_lockout_temp, ideal_mat):
+  oat = np.arange(-37, 111, 1)
+  MAT_at_min_OA = rat*(1 - min_outside_air) + min_outside_air*oat
+  MAT = np.zeros((len(oat), 1))
+  for i, temp in enumerate(oat):
+    if (MAT_at_min_OA[i] < ideal_mat or temp <= low_lockout_temp or temp >= high_lockout_temp):
+       MAT[i] = MAT_at_min_OA[i]
+    elif (temp < ideal_mat):
+        MAT[i] = ideal_mat
+    else:
+        MAT[i] = oat[i]
+  rand_data = GenerateRandomData(oat, MAT)
+  plt.rcParams.update({'font.size': 30})
+  plt.figure()
+  plt.plot(oat, MAT)
+  plt.scatter(rand_data[:,0], rand_data[:,1], color = "green")
+  plt.xlabel("Outside Air Temperature (Farenheit)")
+  plt.ylabel("Mixed Air Temperature (Farenheit)")
+  plt.title("Ideal Economizer Plot")
+  plt.show()
+
+def GenerateRandomData(OAT, MAT):
+  max_idx = len(OAT)
+  data = np.zeros((len(OAT), 2))
+  for _ in range(10000*max_idx):
+    rand_num = random.gauss(74, max_idx/12)
+    while (rand_num < 0 or rand_num > max_idx):
+      rand_num = random.gauss(0, max_idx/12)
+    rand_idx = int(rand_num)
+    # use range of maybe +-3 degrees?
+    rand_data = random.random()*5 + MAT[rand_idx] - 2.5
+    data[rand_idx][1] = rand_data
+    data[rand_idx][0] = OAT[rand_idx]
+  mask = data[:, 1] > 40
+  data = data[mask]
+  
+  return data
+
+PlotIdealCurve(0.2, 72, -30, 70, 55)
+
+
