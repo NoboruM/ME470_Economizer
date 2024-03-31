aa8a0dfbc2867fcc457a010a1a27adb594c12e4d
KLuburic <123262244+KLuburic@users.noreply.github.com>
2024-03-31 11:48:43 -0500

    Add files via upload
    Updated some of the code

--- Chip Test Codes/ADC_test/ADC_test.ino
+++ Chip Test Codes/ADC_test/ADC_test.ino
@@ -7,23 +7,23 @@ ADS1115 ADS(0x48);
 void setup() 
 {
   Serial.begin(115200);
+
+  //while(1) {Serial.println("Test!"); delay(1000);}
  
   ADS.begin();
+  ADS.setGain(0);
 }
  
  
 void loop() 
 {
-  ADS.setGain(0);
- 
-  int16_t val_1 = ADS.readADC(1);  
-  int16_t val_2 = ADS.readADC(2);  
+  int16_t val_3 = ADS.readADC(3);
+  int16_t val_0 = ADS.readADC(0);  
   float f = ADS.toVoltage(1);  // voltage factor
-  float Vin = val_2 * f;
-  float Vout = val_1 * f;
+  float Vin = val_0 * f;
+  float Vout = val_3 * f;
   float ohms = R1*(Vin/Vout - 1);
  
-  Serial.print("\tAnalog1: "); Serial.print(val_1); Serial.print('\t'); Serial.print(Vout, 3);  Serial.print("V\t"); Serial.print(ohms, 2); Serial.println("OHMs");
- 
-  delay(500);
+  Serial.print("Analog4: "); Serial.print(val_3); Serial.print('\t'); Serial.print(Vout, 3);  Serial.print("V\t"); Serial.print(ohms, 2); Serial.println("OHMs");
+  delay(1000);
 }
