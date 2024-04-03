df8200f5e1cc4e067afe03b2f084a12b83037b7f
KLuburic <123262244+KLuburic@users.noreply.github.com>
2024-04-02 23:49:41 -0500

    Add files via upload
    Updated the ADC code again to read all of the inputs

--- Chip Test Codes/ADC_test/ADC_test.ino
+++ Chip Test Codes/ADC_test/ADC_test.ino
@@ -17,13 +17,22 @@ void setup()
  
 void loop() 
 {
-  int16_t val_3 = ADS.readADC(3);
-  int16_t val_0 = ADS.readADC(0);  
+  int16_t val_0 = ADS.readADC(0);
+  int16_t val_1 = ADS.readADC(1);
+  int16_t val_2 = ADS.readADC(2);
+  int16_t val_3 = ADS.readADC(3);  
   float f = ADS.toVoltage(1);  // voltage factor
   float Vin = val_0 * f;
-  float Vout = val_3 * f;
-  float ohms = R1*(Vin/Vout - 1);
- 
-  Serial.print("Analog4: "); Serial.print(val_3); Serial.print('\t'); Serial.print(Vout, 3);  Serial.print("V\t"); Serial.print(ohms, 2); Serial.println("OHMs");
+  float Vout_1 = val_1 * f;
+  float Vout_2 = val_2 * f;
+  float Vout_3 = val_3 * f;
+  float ohms_1 = R1*(Vin/Vout_1 - 1);
+  float ohms_2 = R1*(Vin/Vout_2 - 1);
+  float ohms_3 = R1*(Vin/Vout_3 - 1);
+  
+  Serial.print("Analog1: "); Serial.print(Vout_1, 3);  Serial.print("V\t"); Serial.print(ohms_1, 2); Serial.println("OHMs");
+  Serial.print("Analog2: "); Serial.print(Vout_2, 3);  Serial.print("V\t"); Serial.print(ohms_2, 2); Serial.println("OHMs");
+  Serial.print("Analog3: "); Serial.print(Vout_3, 3);  Serial.print("V\t"); Serial.print(ohms_3, 2); Serial.println("OHMs");
+  Serial.println("");
   delay(1000);
 }
