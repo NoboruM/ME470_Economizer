159d2d8259a9deb93623802ea2d903bcfed67c7c
noborum <noborum2@illinois.edu>
2024-04-11 02:11:34 -0500

    minor testing of local time

--- RaspberryPi/test_files/string_test.py
+++ RaspberryPi/test_files/string_test.py
@@ -1,2 +1,23 @@
-oat = "17.1"
-print(float(oat))
+from time import strftime, localtime, time
+import datetime
+from datetime import datetime
+import calendar
+import pytz
+
+# year = "2024"
+# month = "4"
+# day = "11"
+# hours = "2"
+# minutes = "8"
+# seconds = "42"
+# tz = pytz.timezone("US/Central")
+# date_string = datetime.datetime(int(year), int(month), int(day), int(hours), int(minutes), int(seconds)) 
+# corrected_date = tz.localize(date_string)
+# print("date: ", date_string)
+# epoch_date = calendar.timegm(corrected_date.timetuple())
+# print("epoch_date: ", epoch_date)
+# print("actual: ", int(time()))
+# print("error: ", (int(time()) - epoch_date)/3600)
+
+
+print((1712819166 - 1712801100)/(3600))
